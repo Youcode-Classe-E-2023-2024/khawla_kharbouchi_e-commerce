97f1dff58e38322a4355a7af971fce725dea0dbf
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-01-30 10:52:45 +0100

    authentification

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -1,12 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class authController extends Controller
+class AuthController extends Controller 
 {
     public function register(){
         return view('auth/register');
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::controller(authController::class)->group(function (){
+    Route::get('register','register')->name('register');
 });
