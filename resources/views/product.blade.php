450c174bfe8f62726459771e268334ac0f240636
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-01-31 11:07:40 +0100

    front product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -10,11 +10,11 @@
     <title>shop</title>
 </head>
 <body>
-    <div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-        <div class="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+    <div class="grid min-h-[140px] w-full place-items-center  rounded-lg p-6 lg:overflow-visible">
+        <div class="-m-6 max-h-[768px] w-[calc(100%+48px)] ">
           <nav
-            class="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-white border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
-            <div class="flex items-center justify-between text-blue-gray-900">
+            class="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-black bg-white border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
+            <div class="flex items-center justify-between text-black">
               <a href="#"
                 class="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
                 Material Tailwind
@@ -22,22 +22,22 @@
               <div class="flex items-center gap-4">
                 <div class="hidden mr-4 lg:block">
                   <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-black">
                       <a href="#" class="flex items-center">
                         Pages
                       </a>
                     </li>
-                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-black">
                       <a href="#" class="flex items-center">
                         Account
                       </a>
                     </li>
-                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-black">
                       <a href="#" class="flex items-center">
                         Blocks
                       </a>
                     </li>
-                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-black">
                       <a href="#" class="flex items-center">
                         Docs
                       </a>
@@ -45,16 +45,12 @@
                   </ul>
                 </div>
                 <div class="flex items-center gap-x-1">
-                  <button
-                    class="hidden px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
-                    type="button">
-                    <span>Log In</span>
-                  </button>
-                  <button
+                    <button
                     class="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
                     type="button">
-                    <span>Sign in</span>
+                    <span>add product</span>
                   </button>
+
                 </div>
                 <button
                   class="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
