c9679892d96a76ad96585f4dbfb3a30f7847a031
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-01 20:36:31 +0100

    modal de description

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -136,13 +136,25 @@
                             <del>
                                 <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
                             </del>
-                            <button class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                            <button class="ml-auto" onclick="document.getElementById('modal_{{ $product->id }}').showModal()"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                     fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16" >
                                     <path fill-rule="evenodd"
                                         d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
                                     <path
                                         d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                                 </svg></button>
+                                <dialog id="modal_{{ $product->id }}" class="modal">
+                                    <div class="modal-box w-11/12 max-w-5xl">
+                                      <h3 class="font-bold text-lg">Hello!</h3>
+                                      <p class="py-4">Click the button below to close</p>
+                                      <div class="modal-action">
+                                        <form method="dialog">
+                                          <!-- if there is a button, it will close the modal -->
+                                          <button class="btn">Close</button>
+                                        </form>
+                                      </div>
+                                    </div>
+                                  </dialog>
                         </div>
                     </div>
                 </a>
