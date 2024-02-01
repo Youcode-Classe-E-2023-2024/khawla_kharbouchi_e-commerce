730db7bf2e9f140ba8cfee1ed1dfb4d5f42250f9
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-01 17:34:37 +0100

    changement de database

--- app/Http/Controllers/proController.php
+++ app/Http/Controllers/proController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Storage;
+use App\Models\product;
 use Illuminate\Http\Request;
 
 class proController extends Controller
@@ -11,9 +12,28 @@ class proController extends Controller
     {
         return view('product');
     }
+
     public function productPost(Request $request)
 {
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'price' => 'required|numeric',
+        'description' => 'nullable|string',
+        'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ]);
+    $product = new Product();
+    $product->name = $request->name;
+    $product->price = $request->price;
+    $product->description = $request->description;
 
+    if ($request->hasFile('image')) {
+        $file = $request->file('image');
+        $filename = time() . '.' . $file->getClientOriginalExtension();
+        $file->storeAs('public/images', $filename);
+
+    }
+    $product->save();
+    return back()->with('success', 'Product ajouté avec succès');
 }
 
 }
--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->string('name');
             $table->integer('price');
             $table->text('description');
